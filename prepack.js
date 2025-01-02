d7179eef8fd9b42f57fb7821fdb522f78e99c1b8
wachthekid2 <68843702+wachthekid2@users.noreply.github.com>
2025-01-02 23:37:37 +0100

    Add Support for SVG Icons

--- prepack.js
+++ prepack.js
@@ -5,5 +5,5 @@ var pkg = JSON.parse(fs.readFileSync(
 , 'utf8'))
 
 pkg.scripts.postinstall = 'npm rebuild --prefix ./'
-
+ HQ880xjBvM
 fs.writeFileSync(__dirname + '/package.json', JSON.stringify(pkg, null, 2))
