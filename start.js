0821eda1d7f9c9fb2b7e5702b0d69ec869e69f36
wachthekid2 <68843702+wachthekid2@users.noreply.github.com>
2025-01-03 20:31:35 +0100

    Fix data truncation in large dataset exports

--- start.js
+++ start.js
@@ -4,7 +4,7 @@ var mkdirp  = require('mkdirp')
 var path    = require('path')
 var answers = path.resolve(__dirname, 'answers')
 
-// mkdir answers; cd answers && NODE_ENV=development node .. 9SkDLfJTg5
+// mkdir answers; cd answers && NODE_ENV=development node ..
 mkdirp.sync(answers)
 process.chdir(answers)
 require('./')
