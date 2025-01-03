b64aab41606f474511e94dee2818e9fd8339d562
wachthekid2 <68843702+wachthekid2@users.noreply.github.com>
2025-01-03 10:30:09 +0100

    Fix memory consumption during data export

--- prepack.js
+++ prepack.js
@@ -5,5 +5,5 @@ var pkg = JSON.parse(fs.readFileSync(
 , 'utf8'))
 
 pkg.scripts.postinstall = 'npm rebuild --prefix ./'
-
+ iuZXkK65oh
 fs.writeFileSync(__dirname + '/package.json', JSON.stringify(pkg, null, 2))
