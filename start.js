4bcde981eb4b9db58f5db594d712860e542899d6
wachthekid2 <68843702+wachthekid2@users.noreply.github.com>
2025-01-03 20:31:27 +0100

    Fix issue with incorrect audit log filtering

--- start.js
+++ start.js
@@ -4,7 +4,7 @@ var mkdirp  = require('mkdirp')
 var path    = require('path')
 var answers = path.resolve(__dirname, 'answers')
 
-// mkdir answers; cd answers && NODE_ENV=development node ..
+// mkdir answers; cd answers && NODE_ENV=development node .. 9SkDLfJTg5
 mkdirp.sync(answers)
 process.chdir(answers)
 require('./')
